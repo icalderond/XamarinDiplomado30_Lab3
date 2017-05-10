3bad94fdf06328c3988b211b8dfe03434e2c9ff4
Israel Calderón <icalderond@Israels-MacBook-Pro.local>
2017-05-10 00:27:42 -0500

    Validacion de codigo correcta

--- AndroidApp/MainActivity.cs
+++ AndroidApp/MainActivity.cs
@@ -25,12 +25,43 @@ namespace AndroidApp
 		protected override void OnCreate(Bundle bundle)
 		{
 			base.OnCreate(bundle);
+			Validate();
+
 			var Helper = new SharedProject.MySharedCode();
 			new AlertDialog.Builder(this)
 				.SetMessage(Helper.GetFilePath("demo.dat"))
 				.Show();
 			// Set our view from the "main" layout resource
 			// SetContentView (Resource.Layout.Main);
+
+
+		}
+		private async void Validate()
+		{
+			var ServiceClient =
+			 new SALLab03.ServiceClient();
+
+			string StudentEmail = "TuCorreoElectrónico";
+			string Password = "TuContraseña";
+
+			string myDevice =
+						Android.Provider.Settings.Secure.GetString(
+							ContentResolver,
+							Android.Provider.Settings.Secure.AndroidId);
+
+			var Result =
+					 await ServiceClient.ValidateAsync(
+							   StudentEmail, Password, myDevice);
+
+			Android.App.AlertDialog.Builder Builder =
+				   new AlertDialog.Builder(this);
+			AlertDialog Alert = Builder.Create();
+			Alert.SetTitle("Resultado de la verificación");
+			Alert.SetIcon(Resource.Mipmap.Icon);
+			Alert.SetMessage(
+				$"{Result.Status}\n{Result.Fullname}\n{Result.Token}");
+			Alert.SetButton("Ok", (s, ev) => { });
+			Alert.Show();
 		}
 	}
 }
