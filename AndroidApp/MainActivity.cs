5f30308c79749bd2133cd528a40c9a13e75a2be9
Israel Calderón <icalderond@Israels-MacBook-Pro.local>
2017-05-09 22:48:15 -0500

    Agregando codigo para mostrar ruta en android

--- AndroidApp/MainActivity.cs
+++ AndroidApp/MainActivity.cs
@@ -9,18 +9,28 @@ namespace AndroidApp
 	{
 		int count = 1;
 
-		protected override void OnCreate(Bundle savedInstanceState)
+		//protected override void OnCreate(Bundle savedInstanceState)
+		//{
+		//	base.OnCreate(savedInstanceState);
+
+		//	// Set our view from the "main" layout resource
+		//	SetContentView(Resource.Layout.Main);
+
+		//	// Get our button from the layout resource,
+		//	// and attach an event to it
+		//	Button button = FindViewById<Button>(Resource.Id.myButton);
+
+		//	button.Click += delegate { button.Text = $"{count++} clicks!"; };
+		//}
+		protected override void OnCreate(Bundle bundle)
 		{
-			base.OnCreate(savedInstanceState);
-
+			base.OnCreate(bundle);
+			var Helper = new SharedProject.MySharedCode();
+			new AlertDialog.Builder(this)
+				.SetMessage(Helper.GetFilePath("demo.dat"))
+				.Show();
 			// Set our view from the "main" layout resource
-			SetContentView(Resource.Layout.Main);
-
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button>(Resource.Id.myButton);
-
-			button.Click += delegate { button.Text = $"{count++} clicks!"; };
+			// SetContentView (Resource.Layout.Main);
 		}
 	}
 }
