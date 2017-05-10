b567e40d2fa71c1acab3d7ae30cb66ac03fedbbb
Israel Calderón <icalderond@Israels-MacBook-Pro.local>
2017-05-09 22:38:31 -0500

    Agregando condicional para la rura de cada plataforma

---
+++ SharedProject/MySharedCode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace SharedProject
+{
+	public class MySharedCode
+	{
+		public MySharedCode()
+		{
+		}
+		public string GetFilePath(string fileName)
+		{
+
+#if WINDOWS_UWP
+            var FilePath = Path.Combine(
+                Windows.Storage.ApplicationData.Current.LocalFolder.Path,
+                filename);
+#if __ANDROID__
+               string LibraryPath =
+                   Environment.GetFolderPath(
+                       Environment.SpecialFolder.Personal); ;
+               var FilePath = Path.Combine(LibraryPath, fileName);
+#endif
+#endif
+			return FilePath;
+		}
+	}
+}
